13f9012079c2c8b7f65276f93df4f80be683271d
Redward <collardmusic@gmail.com>
2024-10-08 02:21:28 -0700

    endhints!

---
+++ lua/plugins/lsp-utils/endhints.lua
@@ -0,0 +1,19 @@
+return {
+
+  'chrisgrieser/nvim-lsp-endhints',
+  opts = {
+    icons = {
+        type = " ",
+        parameter = " ",
+        offspec = " ", -- hint kind not defined in official LSP spec
+        unknown = " ", -- hint kind is nil
+      },
+      label = {
+        padding = 1,
+        marginLeft = 0,
+        bracketedParameters = true,
+      },
+      autoEnableHints = true,
+  },
+
+}
