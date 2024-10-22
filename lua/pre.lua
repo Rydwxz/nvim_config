71b7475e6eaae6ace132f459f74f3236922ceb77
Redward <collardmusic@gmail.com>
2024-10-22 01:14:43 -0700

    tab=4

--- lua/pre.lua
+++ lua/pre.lua
@@ -11,9 +11,9 @@ vim.o.termguicolors = true
 vim.o.number = true
 
 vim.o.expandtab = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
 
 vim.g.have_nerd_font = true
 
