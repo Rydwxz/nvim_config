ca5968d2a213232f01e814aa0b11d0aa00691971
Redward <collardmusic@gmail.com>
2024-10-22 01:15:27 -0700

    lsp0 kinda works

--- lua/plugins/lsp-utils/lspzero.lua
+++ lua/plugins/lsp-utils/lspzero.lua
@@ -1,125 +1,135 @@
 return {
 
-	{'neovim/nvim-lspconfig',},             -- Required
-	{                                      -- Optional
-		'williamboman/mason.nvim',
-		build = function()
-		pcall(vim.cmd, 'MasonUpdate')
-		end,
-	},
-	{'williamboman/mason-lspconfig.nvim'}, -- Optional
+	{
+    'VonHeikemen/lsp-zero.nvim',
+    lazy = true,
+    config = false,
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    opts = {},
+  },
+  {'williamboman/mason-lspconfig.nvim'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/cmp-buffer'},
+  {'hrsh7th/cmp-path'},
+  {
+    'neovim/nvim-lspconfig',
+    cmd = {'LspInfo', 'LspInstal', 'LspStart'},
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+    },
+    init = function()
+      vim.opt.signcolumn = 'yes'
+    end,
+    config = function()
+      local lsp_def = require 'lspconfig'.util.default_config
+      lsp_def.capabilities = vim.tbl_deep_extend(
+        'force',
+        lsp_def.capabilities,
+        require'cmp_nvim_lsp'.default_capabilities()
+      )
 
-	{'hrsh7th/nvim-cmp',
-		name = 'cmp'},     -- Required
-	{'hrsh7th/cmp-nvim-lsp'}, -- Required
-	{'L3MON4D3/LuaSnip'},     -- Required
-	{'hrsh7th/cmp-buffer'}, -- Required
-	{'hrsh7th/cmp-path'}, -- Required
-
-	{'VonHeikemen/lsp-zero.nvim',
-		dependencies = {
-	    {'ray-x/lsp_signature.nvim'},
-      {'SmiteshP/nvim-navic'},
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-        pcall(vim.cmd, 'MasonUpdate')
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = {buffer = event.buf}
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
         end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-      {'hrsh7th/nvim-cmp', name = 'cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
-      {'hrsh7th/cmp-buffer'}, -- Required
-      {'hrsh7th/cmp-path'}, -- Required
-		},
-
-	config = function()
-		local lsp = require('lsp-zero').preset({'recommended'})
-		local navic = require('nvim-navic')
-		local sig = require('lsp_signature').setup()
-		lsp.on_attach(function(client, bufnr)
-			navic.attach(client, bufnr)
-			require('lsp_signature').on_attach(sig, bufnr)
-			lsp.default_keymaps({buffer = bufnr})
-		end)
-		lsp.setup()
-
-		require('mason').setup()
-		require('mason-lspconfig').setup({
-			ensure_installed = {
-        'bashls',
-				'biome',
-				'clangd',
-				'cmake',
-        'cssls',
-        'dockerls',
-				'gopls',
-				--'hls',
-        'jsonls',
-        'lemminx', --xml
-				'lua_ls',
-				'marksman',
-				'pyre',
-        'quick_lint_js',
-				'reason_ls',
-				'rust_analyzer',
-        'sorbet', --ruby
-				-- 'tsserver',
-        'vimls',
-        'yamlls',
-        'zls',
-			},
-			handlers = {
-				function(server_name)
-                    require('lspconfig')[server_name].setup({})
-				end,
-			},
-		})
-
-
-		local cmp = require('cmp')
-		cmp.setup({
-			mapping = {
-				['<cr>'] = function(fallback)
-					if cmp.visible() then
-						cmp.confirm({select = true})
-					else
-						fallback()
-					end
-				end,
-				['<tab>'] = function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-						cmp.confirm()
-					else
-						fallback()
-					end
-				end,
-				['<a-tab>'] = function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-						cmp.select_next_item()
-						cmp.confirm()
-					else
-						fallback()
-					end
-				end,
-				['<c-tab>'] = function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-						cmp.select_next_item()
-						cmp.select_next_item()
-						cmp.confirm()
-					else
-						fallback()
-					end
-				end,
-			},
-		})
-	end
-},
+      })
+      require'mason-lspconfig'.setup({
+        ensure_installed = {
+          'bashls',
+          'biome',
+          'clangd',
+          'cmake',
+          'cssls',
+          'dockerls',
+          'gopls',
+          'hls',
+          'jsonls',
+          'lemminx', --xml
+          'lua_ls',
+          'marksman',
+          'pyre',
+          'quick_lint_js',
+          'reason_ls',
+          'rust_analyzer',
+          'sorbet', --ruby
+          'vimls',
+          'yamlls',
+          'zls',
+        },
+        handlers = {
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+        },
+      })
+    end,
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    config = function()
+      local cmp = require 'cmp'
+      cmp.setup({
+        sources = {
+          {name = 'nvim_lsp'},
+          {name = 'buffer'},
+          {name = 'path'},
+        },
+        mapping = {
+          ['<cr>'] = function(fallback)
+            if cmp.visible() then
+              cmp.confirm({select = true})
+            else
+              fallback()
+            end
+          end,
+          ['<tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+              cmp.confirm()
+            else
+              fallback()
+            end
+          end,
+          ['<a-tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+              cmp.select_next_item()
+              cmp.confirm()
+            else
+              fallback()
+            end
+          end,
+          ['<c-tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+              cmp.select_next_item()
+              cmp.select_next_item()
+              cmp.confirm()
+            else
+              fallback()
+            end
+          end,
+        },
+      })
+    end,
+  },
 
 }
