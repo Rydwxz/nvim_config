20ac46c838cab6c885169c5be17965c1424ffb0c
Rydwxz <collard.cameron@gmail.com>
2024-11-25 15:00:59 -0800

    wsl

--- lua/plugins/win-mgt/transparent.lua
+++ lua/plugins/win-mgt/transparent.lua
@@ -1,12 +1,12 @@
 return {
 
-	  'xiyaowong/transparent.nvim',
-	  opts = {
-	  	extra_groups = {
+  'xiyaowong/transparent.nvim',
+  opts = {
+  	extra_groups = {
       "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
       "NvimTreeNormal" -- NvimTree
-      },
-	  },
-	  lazy = false,
+    },
+  },
+  lazy = false,
 
 }
