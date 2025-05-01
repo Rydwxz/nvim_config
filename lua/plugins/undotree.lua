b445a1d747296677a891db022f666a83a13c076c
Redward <collardmusic@gmail.com>
2025-05-01 16:17:26 -0700

    undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,10 +1,10 @@
 return {
---
--- 	'mbbill/undotree',
--- 	config = function()
--- 		vim.g.undotree_SetFocusWhenToggle = 1
--- 		vim.g.undotree_ShortIndicators = 1
--- 		vim.g.undotree_SplitWidth = 34
--- 	end,
---
+
+	'mbbill/undotree',
+	config = function()
+		vim.g.undotree_SetFocusWhenToggle = 1
+		vim.g.undotree_ShortIndicators = 1
+		vim.g.undotree_SplitWidth = 34
+	end,
+
 }
