54dca2904d51a53e247e401c650d4cabc80a58a1
Redward <collardmusic@gmail.com>
2025-05-01 16:15:41 -0700

    ufo

--- lua/pre.lua
+++ lua/pre.lua
@@ -1,24 +1,19 @@
-vim.o.timeoutlen = 4000
-
+vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.g.have_nerd_font = true
 vim.g.clipboard = unnamedplus
-
+vim.o.timeoutlen = 4000
 vim.o.equalalways = false
 vim.o.background = dark
 vim.o.cursorline = true
-
 vim.o.termguicolors = true
-
 vim.o.number = true
-
 vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
-
-vim.g.have_nerd_font = true
-
 vim.o.updatetime = 500
-
 vim.o.scrolloff = 4
-
 vim.o.makeprg = cargo
