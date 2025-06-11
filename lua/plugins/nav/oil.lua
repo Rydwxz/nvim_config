15a1ce580eda18a306e992ae3c7e79c21a7f5ea4
Redward <collardmusic@gmail.com>
2025-06-11 11:49:37 -0700

    oil

--- lua/plugins/nav/oil.lua
+++ lua/plugins/nav/oil.lua
@@ -1,23 +1,25 @@
 return {
 
-	'stevearc/oil.nvim',
-	dependencies = 'nvim-tree/nvim-web-devicons',
-	opts = {
-		default_file_explorer = true,
-    delete_to_trash = true,
-		columns = {
-			'permissions',
-			'icon',
-			'size',
-			'mtime',
-		},
-		win_options = {
-			signcolumn = 'yes',
-		},
-		view_options = {
-			show_hidden = true,
-		},
-	},
+    'stevearc/oil.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        columns = {
+            'permissions',
+            'icon',
+            'size',
+            'mtime',
+        },
+        win_options = {
+            signcolumn = 'yes',
+        },
+        view_options = {
+            show_hidden = true,
+        },
+        skip_confirm_for_simple_edits = true,
+        watch_for_changes = true,
+    },
 
 
 }
