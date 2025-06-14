00b8ba15623ae4a373f138e1225d7b20ead62e27
Redward <collardmusic@gmail.com>
2025-06-14 01:44:54 -0700

    expand telescope layout

--- lua/plugins/nav/telescope.lua
+++ lua/plugins/nav/telescope.lua
@@ -1,18 +1,20 @@
 return {
 
-	'nvim-telescope/telescope.nvim',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-	},
-	opts = {
-		defaults = {
-			path_display = { shorten = 3, },
-		},
-		pickers = {
-			find_files = {
-				hidden = true,
-			},
-		},
-	},
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
+    opts = {
+        defaults = {
+            path_display = { shorten = 3, },
+            layout_strategy = 'horizontal',
+            layout_config = { height = 0.99, width = 0.99 },
+        },
+        pickers = {
+            find_files = {
+                hidden = true,
+            },
+        },
+    },
 
 }
