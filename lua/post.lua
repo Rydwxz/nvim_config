a0a0a451d38f99bdd682e798bba927369445229e
Redward <collardmusic@gmail.com>
2025-06-18 01:14:21 -0700

    some commented broot autocmds

--- lua/post.lua
+++ lua/post.lua
@@ -10,4 +10,21 @@ vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   end,
 })
 
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--   group = vim.api.nvim_create_augroup("Broot start directory", { clear = true }),
+--   pattern = "*",
+--   callback = function()
+--     vim.schedule(function()
+--       -- Early return if netrw or not a directory
+--       if vim.bo[0].filetype == "netrw" or vim.fn.isdirectory(vim.fn.expand("%:p")) == 0 or vim.api.nvim_buf_get_option(0, 'buflisted') == false then
+--         return
+--       end
+--
+--       -- vim.api.nvim_buf_set_option(0, "bufhidden", "wipe")
+--
+--       require("fnc").term.find_file()
+--     end)
+--   end,
+-- })
+
 vim.lsp.enable('nushell')
